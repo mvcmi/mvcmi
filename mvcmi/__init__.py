ff8f2a6d0490b7fc3990d45d6712fc288f77de36
Mainak Jas <mainakjas@gmail.com>
2024-01-17 11:26:34 -0500

    ENH: fixes

--- mvcmi/__init__.py
+++ mvcmi/__init__.py
@@ -1,4 +1,4 @@
-from .algo import compute_cmi, compute_ccoef_pca
-from .pca import reduce_dim
+from .algo import compute_cmi, compute_ccoef_pca, z_score
+from .pca import generate_null_dist
 
 __version__ = "0.1.dev0"
